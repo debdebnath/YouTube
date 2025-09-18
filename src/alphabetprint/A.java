41a07f1e78bc4dddc754e0fbbccbab3e1fd5d0b9
Debajyoti <dev.demo08@gmail.com>
2025-09-18 08:10:35 +0530

    add new package alphabetPrint

---
+++ src/alphabetprint/A.java
@@ -0,0 +1,19 @@
+package alphabetprint;
+
+public class A {
+
+	public static void main(String[] args) {
+		int num=5;
+		for (int i = 0; i < num; i++) {
+			for(int j=0;j<num;j++)
+			{
+				if(i==0||j==0 )
+				{
+					
+				}
+			}
+			
+		}
+	}
+
+}
